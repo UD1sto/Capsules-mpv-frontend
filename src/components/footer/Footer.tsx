7a677c16256abce865626f6f29c3944eefd3f4a7
khrystynaDm1307 <53223985+khrystynaDm1307@users.noreply.github.com>
2023-01-26 11:17:21 +0200

    export footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Stack, Typography } from "@mui/material";
-import styles from "./styles.module.css";
+import styles from "@/styles/Footer.module.css";
 
 export function Footer() {
   return (
